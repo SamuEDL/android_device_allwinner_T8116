35a2eba749b7fac339fe1280667a672bd8452cd8
SamuEDL <59443298+SamuEDL@users.noreply.github.com>
2022-10-05 16:28:08 +0200

    Update omni_T8116.mk

--- omni_T8116.mk
+++ omni_T8116.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from T8100 device
 $(call inherit-product, device/allwinner/T8116/device.mk)
